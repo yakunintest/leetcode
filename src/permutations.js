a2a0b9d6738a94e48d555d0afb0394b02d995d97
Arkadiy Yakunin <a.yakunineg@gmail.com>
2021-09-07 10:27:13 +0300

    debugg

--- src/permutations.js
+++ src/permutations.js
@@ -11,20 +11,21 @@ const permute = (nums) => {
     }
     for (let i = 0; i < nums.length; i++) {
       arr.push(nums[i]); // 0
-
+      console.log('1==>', arr);
       let decreased = nums.filter((n, index) => index !== i);
-
+      console.log('2==>', decreased);
       helper(decreased, arr);
-
+      console.log('3==>', arr)
       arr.pop(); // unchoose
 
     }
   }
 
   helper(nums, []);
+  console.log('4==>', result);
   return result;
 };
 
 console.log(permute([1, 2, 3]))
-console.log(permute([0, 1]))
-console.log(permute([1]));
+// console.log(permute([0, 1]))
+// console.log(permute([1]));
